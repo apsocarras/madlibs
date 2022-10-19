5ae728187849c307a9354331f457a1b69c9bf204
Alejandro Socarras <apsocarras@gmail.com>
2022-10-19 14:09:14 -0600

    Increase font size with each submit event
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

--- js/scripts-shout.js
+++ js/scripts-shout.js
@@ -1,10 +1,19 @@
 // UI Logic 
 
 function getAndSetShout() {
-  const shoutInput = document.getElementById("shout-input").value;
-
-  document.querySelector("p").innerText = shoutInput.toUpperCase();
+  const shoutInput = document.getElementById("shout-input").value
+  document.querySelector("p").innerText = shoutInput.toUpperCase()
 }
+
+let font = 1;
+function increaseFontSize() {
+  // let font = parseInt(document.querySelector("p").style.fontSize.slice(1))
+  font += 5
+  console.log(font)
+  document.querySelector("p").style.fontSize = font.toString() + "px"
+  // console.log(document.querySelector("p").style.fontSize)
+}
+
 
 window.onload = function() {
   // console.log("This script is running!");
@@ -12,5 +21,6 @@ window.onload = function() {
   form.onsubmit = function(event) {
     event.preventDefault();
     getAndSetShout();
+    increaseFontSize();
   }
 }
